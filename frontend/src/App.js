5cad5f425738e9b96e2d527450ac1bb379ca4ca6
Shanthu Shanthu <ssanthujan@yahoo.com>
2023-12-23 18:54:52 +0530

    netifly stuffs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
-
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/home';
-
-import AboutUs from './pages/aboutUs/aboutUs';
-import HowWeWork from './pages/howWeWork/howWeWork';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+import Portfolio from "./pages/portfolio/portfolio";
+import AboutUs from "./pages/aboutUs/aboutUs";
+import HowWeWork from "./pages/howWeWork/howWeWork";
 function App() {
   return (
-    <div className="App">
-     < Router>
-     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='how-we-work' element={<HowWeWork/>}/>
-      <Route path='about-us' element={<AboutUs/>}/>
-      </Routes>
-      
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='how-we-work' element={<HowWeWork />} />
+          <Route path='about-us' element={<AboutUs />} />
+          <Route path='portfolio' element={<Portfolio />} />
+        </Routes>
       </Router>
     </div>
   );
