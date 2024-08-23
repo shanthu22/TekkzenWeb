6af4ca8a9c4f8a5259aaf2ca64f3235c6c4c0fc0
Shanthu Shanthu <ssanthujan@yahoo.com>
2024-08-24 03:49:44 +0530

    check cicd trigger

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -67,7 +67,7 @@ const Home = () => {
     <div style={{ font: "16px  Roboto, Helvetica,Arial, sans-serif " }}>
       <TZnavbar />
       <h1 style={{ padding: "25px 20px", fontWeight: "700" }}>
-        We bring your concepts and ideas to life
+        FIXXXXXXXXXXXXXXXX
       </h1>
 
       <p style={{ textAlign: "center", margin: "12px" }}>
